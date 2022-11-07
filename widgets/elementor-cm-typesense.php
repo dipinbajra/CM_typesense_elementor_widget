fec6d6376d6e1bf10bb63594653c63b99957492b
dipinbajra <bajradipin98@gmail.com>
2022-11-07 17:19:15 +0545

    Added controls to the widget

--- widgets/elementor-cm-typesense.php
+++ widgets/elementor-cm-typesense.php
@@ -77,8 +77,8 @@ class Elementor_CM_Typesense extends \Elementor\Widget_base{
 		$this->start_controls_section(
 			'content_section',
 			[
-				'label' => esc_html__( 'Layout', 'textdomain' ),
-				'tab' => \Elementor\Controls_Manager::TAB_LAYOUT,
+				'label' => esc_html__( 'Content', 'CM Typesense' ),
+				'tab' => \Elementor\Controls_Manager::TAB_CONTENT,
 			]
 			);
 			
@@ -104,25 +104,166 @@ class Elementor_CM_Typesense extends \Elementor\Widget_base{
 						'size' => '',
 					],
 					'selectors' => [
-						'{{WRAPPER}} .ais-SearchBox-form' => 'width: {{SIZE}}{{UNIT}};',
+						'{{WRAPPER}} .cmswt-SearchBox' => 'width: {{SIZE}}{{UNIT}};',
 					],
 					],
 					
+				);
+				$this->add_control(
+					'placeHolder',
+					[
+						'type' => \Elementor\Controls_Manager::TEXT,
+						'label' => esc_html__( 'Placeholder', 'CM Typesense' ),
+						'placeholder' => esc_html__( 'Enter your placeholder', 'CM Typesense' ),
+						'default' => 'Search for...',
+					]
+				);
+				$this->add_control(
+					'columns',
+					[
+						'type' => \Elementor\Controls_Manager::NUMBER,
+						'label' => esc_html__( 'Column', 'CM Typesense'),
+						'default' =>3,
+					]
 					);
-			
-	}			
-		
+				$this->add_control(
+					'post_type',
+					[
+						'type' => \Elementor\Controls_Manager::SELECT2,
+						'label' => esc_html__( 'Post Type', 'CM Typesense' ),							
+						'options' => [
+							'page,post' =>esc_html__('All', 'CM Typesense'),
+							'page' => esc_html__( 'page', 'CM Typesense' ),
+							'post' => esc_html__( 'post', 'CM Typesense' ),
+						],
+						'default' => 'page,post',
+					]
+				);
+				
+				$this->add_control(
+					'filter',
+					[
+						'type' => \Elementor\Controls_Manager::SWITCHER,
+						'label' => esc_html__( 'Filter', 'CM Typesense' ),
+						'label_on' => esc_html__( 'show', 'cm-typesense' ),
+						'label_off' => esc_html__( 'hide', 'cm-typesense' ),
+						'return_value' => 'yes',
+						'default' => '',
+					
+					],
+				);
 
+				$this->add_control(
+					'post_per_page',
+					[
+						'type' => \Elementor\Controls_Manager::NUMBER,
+						'label' => esc_html__( 'Post per page', 'CM Typesense'),
+						'default' =>3,
+				
+					]
+				);	
 
+				$this->add_control(
+					'sort_by',
+					[
+						'type' => \Elementor\Controls_Manager::SWITCHER,
+						'label' => esc_html__( 'Sort By', 'CM Typesense' ),
+						'label_on' => esc_html__( 'show', 'cm-typesense' ),
+						'label_off' => esc_html__( 'hide', 'cm-typesense' ),
+						'return_value' => 'yes',
+						'default' => '',
+					
+					],
+				);
+
+				$this->add_control(
+					'pagination',
+					[
+						'type' => \Elementor\Controls_Manager::SWITCHER,
+						'label' => esc_html__( 'Pagination', 'CM Typesense' ),
+						'label_on' => esc_html__( 'show', 'cm-typesense' ),
+						'label_off' => esc_html__( 'hide', 'cm-typesense' ),
+						'return_value' => 'yes',
+						'default' => '',
+					
+					],
+				);
+
+				$this->add_control(
+					'sticky_first',
+					[
+						'type' => \Elementor\Controls_Manager::SWITCHER,
+						'label' => esc_html__( 'Sticky First', 'CM Typesense' ),
+						'label_on' => esc_html__( 'yes', 'cm-typesense' ),
+						'label_off' => esc_html__( 'no', 'cm-typesense' ),
+						'return_value' => 'yes',
+						'default' => 'no',
+					
+					],
+				);
+
+				$this->add_control(
+					'custom_class',
+					[
+						'type' => \Elementor\Controls_Manager::TEXT,
+						'label' => esc_html__( 'Custom Class', 'CM Typesense' ),
+					]
+				);			
+							
+		$this->end_controls_section();
+
+		$this->start_controls_section(
+			'style_section',
+			[
+				'label' => esc_html__('Style','CM Typesense'),
+				'tab' => \Elementor\Controls_Manager::TAB_STYLE,
+				
+			]
+		);			
+	}
+	
 	protected function render() {
 		
+		$settings = $this->get_settings_for_display();
 
-       echo do_shortcode('[cm_typesense_search placeholder="Search for" columns="3" post_types="page,post" filter="show"  per_page="3"  sortby="show" pagination="show" query_by="post_title,post_content" sticky_first="no"]');
+		$placeHolder = $settings['placeHolder'];
+
+		$columns = $settings['columns']; 
+
+		$post_type = $settings['post_type'];
+
+		$filter = $settings['filter'];
+		if ($filter){
+			$filter = 'show';
+		} 
+		else{
+			 $filter='hide';
+		}
+
+		$post_per_page = $settings['post_per_page'];
 		
+		$sort_by = $settings['sort_by'];
+		if ($sort_by ){
+			$sort_by  = 'show';
+		} 
+		else{
+			 $sort_by ='hide';
+		}
+
+		$pagination = $settings['pagination'];
+		if ($pagination){
+			$pagination = 'show';
+		} 
+		else{
+			 $pagination='hide';
+		}
+
+		$sticky_first = $settings['sticky_first'];
+
+		$custom_class = $settings['custom_class'];
+
+    	echo do_shortcode('[cm_typesense_search placeholder="'.$placeHolder.'" columns="'.$columns.'" post_types="'.$post_type.'" filter="'.$filter.'" custom_class="'.$custom_class.'" per_page="'.$post_per_page.'"  sortby="'.$sort_by.'" pagination="'.$pagination.'" query_by="post_title,post_content" sticky_first="'.$sticky_first.'"]');
+
 	}
-
-    
-
-
 }
 
